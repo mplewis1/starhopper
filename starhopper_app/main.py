31ae0657ffd2937dd430758865f37daa6e32404e
Your Name <matthewpaullewis@gmail.com>
2023-10-22 19:01:43 -0400

    Main

---
+++ starhopper_app/main.py
@@ -0,0 +1,7 @@
+def main():
+    query = input("Starhopper> ")
+    # TODO: Process the query with LLaMa-2 and search functions
+    print(f"You searched for: {query}")
+
+if __name__ == "__main__":
+    main()
