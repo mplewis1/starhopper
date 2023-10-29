3149c3613f8ecb72fd7bf5703fa4c1cf6656c79a
Your Name <matthewpaullewis@gmail.com>
2023-10-29 18:07:23 -0400

    Cleared to start from scratch

--- starhopper_app/cohere.py
+++
@@ -1,2 +0,0 @@
-import cohere
-co = cohere.Client('cUkUMhISEr8QsUhZ8uaVMxZtdL3UJrlaESCyNtHR')
--- starhopper_app/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    query = input("Starhopper> ")
-    print(f"You searched for: {query}")
-
-if __name__ == "__main__":
-    main()
