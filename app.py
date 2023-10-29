4cfea4a284b0bd96efe32731f1548d356059d972
Your Name <matthewpaullewis@gmail.com>
2023-10-29 18:45:48 -0400

    Working on it

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import cohere
+from integrations.box import search_box
 
-co - cohere.Client('cUkUMhISEr8QsUhZ8uaVMxZtdL3UJrlaESCyNtHR')
+co = cohere.Client('cUkUMhISEr8QsUhZ8uaVMxZtdL3UJrlaESCyNtHR')
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Starhopper!"
+    return render_template('index.html')
 
 def cohere_process(question):
     # Cohere API functionality
@@ -18,6 +19,15 @@ def cohere_process(question):
 def query():
     data = request.json
     question = data.get("question")
+    box_developer_token = 'n3sDfWnjXDnjleep1OuJ2lewfj6DBhSU'
+    box_results = search_box(question, box_developer_token)
+
+    if box_results and box_results['entries']:
+        first_result - box_results['entries'][0]
+        file_info = f"File Name: {first_result['name']} (ID: {first_result['id']})"
+    else:
+        file_info = "No results found."
+    
     answer = cohere_process(question)
     return jsonify({"answer": "This is a placeholder response", "source": "Placeholder source"})
 
--- integrations/box.py
+++ integrations/box.py
@@ -1,4 +1,20 @@
 import requests
 
 def search_box(query, access_token):
-    
+    url = "https://api.box.com/2.0/search"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    params = {
+        "query": query,
+        "type": "file",
+        "content_types": "name,description,file_content",
+        "limit": 5
+    }
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        print(f"Message: {response.text}")
+        return None
